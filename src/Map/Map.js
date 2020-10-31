eec5672361a58bf07abb57a7614d89ffcf4148fe
Rodrigo Nascimento <nas.rodrigorj@gmail.com>
2020-10-31 12:12:32 -0500

    Add map zoom and move

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,96 +2,130 @@ import React, { useState, useRef, useEffect } from 'react';
 import MapImg from '../img/floor-plant.gif';
 
 const Map = props => {
-    
+
     const canvasRef = useRef();
+    const zoomIn = useRef();
+    const zoomOut = useRef();
+
+    let scaleMultiplier = 0.8;
     
-    const mapImg = new Image();
-    mapImg.src = MapImg;
-
-    let mapImgWInit = 0;
-    let mapImgHInit = 0;
-    let mapImgW = 0;
-    let mapImgH = 0;
-    let mapImgX = 0;
-    let mapImgY = 0;
-    let increase;
-    let decrease;
-
-    let [increaseState, setIncreaseState] = useState(increase = 1.2);
-    let [decreaseState, setDecreaseState] = useState(decrease = 1.2);
-    
-    const zoomIn = () => {
-        setIncreaseState(increase = 1.2);
-        setDecreaseState(decrease = 0);
-        reDraw(increaseState, decreaseState);
-    };
-
-    const zoomOut = () => {
-        setDecreaseState(decrease = 1.2);
-        setIncreaseState(increase = 0);
-        reDraw(increaseState, decreaseState);
-    };
-
-    const reDraw = props => { 
-        // console.log(increaseState);
-        // console.log(decreaseState);
+    const getCanvas = () => {
         const canvas = canvasRef.current;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        const context = canvas.getContext('2d');
-        
-        mapImgWInit = +mapImg.naturalWidth;
-        mapImgHInit = +mapImg.naturalHeight;
-        if(increase > 0){
-            console.log('got in');
-            console.log(increaseState);
-            mapImgW = +((mapImgWInit / 3).toFixed() * increaseState);
-            mapImgH = +((mapImgHInit / 3).toFixed() * increaseState);
-            setIncreaseState(increaseState = increaseState + 1.2);
-            setDecreaseState(decreaseState = decreaseState - 1.2);
-        }else if(+decrease > 0){
-            console.log('got out');
-            console.log(decreaseState);
-            mapImgW = +((mapImgWInit / 3).toFixed() - (((mapImgWInit / 3).toFixed() * decreaseState) - 1.2));
-            mapImgH = +((mapImgHInit / 3).toFixed() - (((mapImgWInit / 3).toFixed() * decreaseState) - 1.2));
-            setDecreaseState(decreaseState = decreaseState + 1.2);
-            setIncreaseState(increaseState = increaseState - 1.2);
-        }      
-        mapImgX = +((window.innerWidth / 2).toFixed() - (mapImgW / 2).toFixed());
-        mapImgY = +((window.innerHeight / 2).toFixed() - (mapImgH / 2).toFixed());
-
-        context.drawImage(mapImg, mapImgX, mapImgY, mapImgW, mapImgH);
-    
+        return canvas;
     };
 
-    const draw = ctx => {
-        mapImg.onload = () => {
-            mapImgWInit = +mapImg.naturalWidth;
-            mapImgHInit = +mapImg.naturalHeight;
-            mapImgW = +(mapImgWInit / 3).toFixed();
-            mapImgH = +(mapImgHInit / 3).toFixed();
-            mapImgX = +((window.innerWidth / 2).toFixed() - (mapImgW / 2).toFixed());
-            mapImgY = +((window.innerHeight / 2).toFixed() - (mapImgH / 2).toFixed());
+    const getContext = () => {
+        const canvas = getCanvas();
+        const context = canvas.getContext('2d');
+        return context;
+    };
 
-            ctx.drawImage(mapImg, mapImgX, mapImgY, mapImgW, mapImgH);
+    const getMapImage = () => {
+        const mapImg = new Image();
+        mapImg.src = MapImg;
+        return mapImg;
+    };
+
+    let [mapImageState] = useState(getMapImage());
+
+    let [scaleState, setScaleState] = useState(0.3);
+
+    let [translateState, setTranslateState] = useState(
+        {
+            x: window.innerWidth / 2,
+            y: window.innerHeight / 2,
         }
+    );
+
+    let [mouseDownState, setMouseDownState] = useState(false);
+
+    let [startDragOffsetState, setStartDragOffsetState] = useState({x: 0, y: 0});
+
+    const zoomInHandle = () => {
+        scaleState /= scaleMultiplier;
+        setScaleState(scaleState);
+        draw();
+    };
+
+    const zoomOutHandle = () => {
+        scaleState *= scaleMultiplier;
+        setScaleState(scaleState);
+        draw();
+    };
+
+    const firstDraw = () => {
+        window.onload = () => {
+            draw();
+        }
+    };
+
+    const getMouseDown = props => {
+        setMouseDownState(mouseDownState = true);
+        console.log(props.clientX);
+        setStartDragOffsetState(
+            startDragOffsetState = {
+                x: props.clientX - translateState.x,
+                y: props.clientY - translateState.y,
+            }
+        );
+    };
+    
+    const getMouseMove = props => {
+        if (mouseDownState) {
+            setTranslateState(
+                translateState = {
+                    x: props.clientX - startDragOffsetState.x,
+                    y: props.clientY - startDragOffsetState.y,
+                }
+            );
+            draw();
+        }
+    };
+
+    const setMouseDonwFalse = () => {
+        setMouseDownState(mouseDownState = false);
+    };
+
+    const draw = () => {
+        const context = getContext();
+        context.translate(translateState.x, translateState.y);
+        context.scale(scaleState, scaleState);
+        let x = -mapImageState.naturalWidth / 2;
+        let y = -mapImageState.naturalHeight / 2;
+        context.drawImage(mapImageState, x, y);
     };
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        const context = canvas.getContext('2d');
-        draw(context);
+        canvas.addEventListener('mousedown', getMouseDown);
+
+        canvas.addEventListener('mouseup', setMouseDonwFalse);
+
+        canvas.addEventListener('mouseover', setMouseDonwFalse);
+
+        canvas.addEventListener('mouseout', setMouseDonwFalse);
+
+        canvas.addEventListener('mousemove', getMouseMove);
+
+        const btnZoomIn = zoomIn.current;
+        btnZoomIn.addEventListener('click', zoomInHandle);
+
+        const btnZoomOut = zoomOut.current;
+        btnZoomOut.addEventListener('click', zoomOutHandle);
+
+        firstDraw();
     }, []);
 
     return <div>
                 <div style={{display:'flex', justifyContent:'center', margin:'10px'}}>
-                    <button onClick={zoomIn}>+</button>
-                    <button onClick={zoomOut}>-</button>
+                    <button ref={zoomIn}>+</button>
+                    <button ref={zoomOut}>-</button>
                 </div>
                 <canvas ref={canvasRef} {...props}/>
             </div>;
-    
+
 }
+
 export default Map;
